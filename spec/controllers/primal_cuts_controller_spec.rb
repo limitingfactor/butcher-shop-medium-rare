f865865ce4725873f714a54a046d8b2b517a3ff5
John Kelly <jkelly0@me.com>
2013-06-15 19:34:10 -0600

    Refactor primal cuts controller to 1 db hit
    I had to keep create primal cut in the top let to avoid having to change the respond_with logic in the controller.  Maybe there is another way but I prefer not to change the code because of a test.

--- spec/controllers/primal_cuts_controller_spec.rb
+++ spec/controllers/primal_cuts_controller_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe PrimalCutsController do
-  let(:animal) { create :animal }
+  let(:animal) { build(:animal, id: 50) }
+  let(:primal_cut) { create(:primal_cut, id: 60) }
   subject { response }
 
   describe "GET 'index'" do
-    before(:each) { get :index, animal_id: animal.to_param, format: :json }
+    before(:each) do
+      mock_primal_cut = mock("PrimalCut")
+      Animal.should_receive(:find).with(animal.to_param).and_return(animal)
+      animal.should_receive(:primal_cuts).and_return(mock_primal_cut)
+      mock_primal_cut.should_receive(:as_json)
+      get :index, animal_id: animal.to_param, format: :json
+    end
 
     its(:body)   { should_not be_empty }
     its(:status) { should be 200 }
@@ -14,7 +21,13 @@ describe PrimalCutsController do
   describe "POST 'create'" do
     context "animal id present" do
       context "correct params" do
-        before(:each) { post :create, animal_id: animal.to_param, primal_cut: attributes_for(:primal_cut), format: :json }
+        before(:each) do
+          mock_primal_cut = mock("PrimalCut")
+          Animal.should_receive(:find).with(animal.to_param).and_return(animal)
+          animal.should_receive(:primal_cuts).and_return(mock_primal_cut)
+          mock_primal_cut.should_receive(:create).with({ "name" => "filet" }).and_return(primal_cut)
+          post :create, animal_id: animal.to_param, primal_cut: attributes_for(:primal_cut), format: :json
+        end
 
         its(:body)    { should_not be_empty }
         its(:status)  { should be 201 }
@@ -22,7 +35,14 @@ describe PrimalCutsController do
       end
 
       context "with bad params" do
-        before(:each) { post :create, animal_id: animal.to_param, primal_cut: { name: nil }, format: :json }
+        before(:each) do
+          mock_primal_cut = mock("PrimalCut")
+          Animal.should_receive(:find).with(animal.to_param).and_return(animal)
+          animal.should_receive(:primal_cuts).and_return(mock_primal_cut)
+          mock_primal_cut.should_receive(:create).with({ "name" => nil }).and_return(mock_primal_cut)
+          mock_primal_cut.should_receive(:errors).twice.and_return({"something" => "is wrong"})
+          post :create, animal_id: animal.to_param, primal_cut: { name: nil }, format: :json
+        end
 
         its(:body)    { should_not be_empty }
         its(:status)  { should be 422 }
@@ -31,16 +51,20 @@ describe PrimalCutsController do
   end
 
   context "existing primal cut" do
-    let(:primal_cut) { create :primal_cut }
-
     describe "GET 'show'" do
-      before(:each) { get :show, id: primal_cut.to_param, format: :json }
+      before(:each) do
+        PrimalCut.should_receive(:find).with(primal_cut.to_param).and_return(primal_cut)
+        get :show, id: primal_cut.to_param, format: :json
+      end
 
       its(:body)    { should_not be_empty }
       its(:status)  { should be 200 }
 
       context "with an unknown primal_cut" do
-        before(:each) { get :show, id: 9999, format: :json }
+        before(:each) do
+          PrimalCut.should_receive(:find).with("9999").and_raise(ActiveRecord::RecordNotFound)
+          get :show, id: 9999, format: :json
+        end
 
         its("body.strip") { should be_empty }
         its(:status)      { should be 404 }
@@ -49,18 +73,25 @@ describe PrimalCutsController do
 
     describe "PUT 'update'" do
       context "with correct params" do
-        before(:each) { put :update, id: primal_cut.to_param, primal_cut: { name: "Something new" }, format: :json }
+        before(:each) do
+          mock_primal_cut = mock("PrimalCut")
+          PrimalCut.should_receive(:find).with(primal_cut.to_param).and_return(primal_cut)
+          primal_cut.should_receive(:update_attributes).with({"name" => "Something new"}).and_return(true)
+          put :update, id: primal_cut.to_param, primal_cut: { name: "Something new" }, format: :json
+        end
 
         its("body.strip") { should be_empty }
         its(:status)      { should be 204 }
-
-        it "updates the primal_cut" do
-          PrimalCut.find(primal_cut.id).name.should == "Something new"
-        end
       end
 
       context "with bad params" do
-        before(:each) { put :update, { id: primal_cut.to_param, primal_cut: { name: nil }, format: :json } }
+        before(:each) do
+          mock_primal_cut = mock("PrimalCut")
+          PrimalCut.should_receive(:find).with(primal_cut.to_param).and_return(primal_cut)
+          primal_cut.should_receive(:update_attributes).with({"name" => "nil"}).and_return(false)
+          primal_cut.should_receive(:errors).twice.and_return({"something" => "is wrong"})
+          put :update, id: primal_cut.to_param, primal_cut: { name: "nil" }, format: :json
+        end
 
         its(:body)    { should_not be_empty }
         its(:status)  { should be 422 }
@@ -68,7 +99,12 @@ describe PrimalCutsController do
     end
 
     describe "DELETE 'destroy'" do
-      before(:each) { delete :destroy, id: primal_cut.to_param, format: :json }
+      before(:each) do
+        mock_primal_cut = mock("PrimalCut")
+        PrimalCut.should_receive(:find).with(primal_cut.to_param).and_return(primal_cut)
+        primal_cut.should_receive(:destroy)
+        delete :destroy, id: primal_cut.to_param, format: :json
+      end
 
       its("body.strip") { should be_empty }
       its(:status)      { should be 204 }
